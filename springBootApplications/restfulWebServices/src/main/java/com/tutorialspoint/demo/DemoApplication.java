9bdb1555d77b8152ce94391337f794d793d1e107
FanJups <jupsfan@gmail.com>
2019-02-26 23:03:29 +0100

    support the Cross-Origin Resource Sharing

--- springBootApplications/restfulWebServices/src/main/java/com/tutorialspoint/demo/DemoApplication.java
+++ springBootApplications/restfulWebServices/src/main/java/com/tutorialspoint/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,5 +16,15 @@ public class DemoApplication {
    @Bean
    public RestTemplate getRestTemplate() {
       return new RestTemplate();
+   }
+
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurerAdapter() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+         }
+      };
    }
 }
