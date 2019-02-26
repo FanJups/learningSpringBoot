678386c64fc3261b62a08527223455b6570a6255
FanJups <jupsfan@gmail.com>
2019-02-26 17:39:17 +0100

    File Handling

---
+++ springBootApplications/restfulWebServices/src/main/java/com/tutorialspoint/demo/controller/FileUploadController.java
@@ -0,0 +1,27 @@
+package com.tutorialspoint.demo.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FileUploadController {
+   @RequestMapping(value = "/upload", method = RequestMethod.POST, 
+      consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+   
+   public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+      File convertFile = new File("/var/tmp/"+file.getOriginalFilename());
+      convertFile.createNewFile();
+      FileOutputStream fout = new FileOutputStream(convertFile);
+      fout.write(file.getBytes());
+      fout.close();
+      return "File is upload successfully";
+   }
+}
