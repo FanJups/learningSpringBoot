5448f8c099cd97685ce2137f2cefc0362f1c9936
FanJups <jupsfan@gmail.com>
2019-02-27 18:58:44 +0100

    Eureka Client

---
+++ springBootApplications/eurekaclient/src/main/java/com/tutorialspoint/eurekaclient/EurekaclientApplication.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+    public String home() {
+      return "Eureka Client application";
+   }
+
+}
