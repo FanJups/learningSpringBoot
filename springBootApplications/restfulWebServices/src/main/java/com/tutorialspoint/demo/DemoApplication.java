3c7e10edc716f719f0b395b9c45d38b1c8496e7f
FanJups <jupsfan@gmail.com>
2019-02-27 14:25:28 +0100

    Scheduling

--- springBootApplications/restfulWebServices/src/main/java/com/tutorialspoint/demo/DemoApplication.java
+++ springBootApplications/restfulWebServices/src/main/java/com/tutorialspoint/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.tutorialspoint.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
    public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
