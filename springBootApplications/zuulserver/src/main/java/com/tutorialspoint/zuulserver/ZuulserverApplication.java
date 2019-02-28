ca43c897f7cdca77302871f0ca2aa07d0a84b29b
FanJups <jupsfan@gmail.com>
2019-02-28 03:51:35 +0100

    Using Zuul server

---
+++ springBootApplications/zuulserver/src/main/java/com/tutorialspoint/zuulserver/ZuulserverApplication.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserverApplication.class, args);
+	}
+
+}
