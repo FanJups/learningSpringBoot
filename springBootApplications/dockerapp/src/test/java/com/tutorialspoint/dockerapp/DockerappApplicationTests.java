038559fe030385e994b66f7b8814d490323b1cca
FanJups <jupsfan@gmail.com>
2019-03-02 16:48:05 +0100

    Creating Docker image

---
+++ springBootApplications/dockerapp/src/test/java/com/tutorialspoint/dockerapp/DockerappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tutorialspoint.dockerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
