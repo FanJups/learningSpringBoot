2c44eee05d38bb559266b86c779c2e37606e717b
FanJups <jupsfan@gmail.com>
2019-03-03 19:01:29 +0100

    Spring Boot - Flyway Database

---
+++ springBootApplications/flywayapp/src/main/java/com/tutorialspoint/flywayapp/FlywayappApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint.flywayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayappApplication.class, args);
+	}
+
+}
