724a045d56bdbb1199ee64651ea1f721b17996af
FanJups <jupsfan@gmail.com>
2019-03-13 00:11:42 +0100

    o7planning example spring boot rest angular js

---
+++ springBootApplications/SpringBootAngularTraining/o7planning/SpringBootAngularJS/src/main/java/org/o7planning/SpringBootAngularJS/SpringBootAngularJsApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.SpringBootAngularJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;   
+
+@SpringBootApplication
+public class SpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularJsApplication.class, args); 
+	}
+
+} 
---
+++ springBootApplications/SpringBootAngularTraining/o7planning/SpringBootAngularJS/src/main/java/org/o7planning/SpringBootAngularJS/controller/MainController.java
@@ -0,0 +1,17 @@
+   package org.o7planning.SpringBootAngularJS.controller;
+
+   import org.springframework.stereotype.Controller;
+   import org.springframework.web.bind.annotation.RequestMapping;
+
+   @Controller
+
+   public class MainController {
+
+   	@RequestMapping("/")
+   	public String welcome()
+   	{
+   		return "index"; 
+   	}
+
+
+   }
