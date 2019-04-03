3ebce5749885dc6d9fd12d713643147661444200
FanJups <jupsfan@gmail.com>
2019-04-03 19:40:03 +0200

    java code geeks spring boot angular js

---
+++ springBootApplications/SpringBootAngularTraining/springboot-angularjs/src/main/java/com/javacodegeeks/examples/springbootangularjs/SpringbootAngularjsApplication.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.examples.springbootangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularjsApplication.class, args);
+	}
+
+}
